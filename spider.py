6f0edde682dcf1297ff51e74f27dd5de2b956fb4
Blueske <hongshaorouzgy@126.com>
2019-06-18 18:23:56 +0800

    maoyan movie top100

---
+++ spider.py
@@ -0,0 +1,55 @@
+import json
+import re
+import time
+
+import requests
+from requests import RequestException
+
+
+def get_one_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            parse_one_page(response.text)
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+
+def parse_one_page(html):
+    pattern = re.compile(
+        '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',
+        re.S
+    )
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'title': item[2].strip(),
+            'image': item[1],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score': item[5] + item[6]
+        }
+
+
+def write_to_json(content):
+    with open('result.txt', 'ab') as f:
+        # print(type(json.dumps(content)))
+        s = json.dumps(content, ensure_ascii=False) + '\n'
+        f.write(s.encode('utf-8'))
+
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_json(item)
+
+
+if __name__ == '__main__':
+    for i in range(10):
+        main(offset=i * 10)
+        time.sleep(1)
